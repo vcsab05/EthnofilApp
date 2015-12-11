c00083018c1257a8289d0cdd27d1cf5c184e570d
vcsab05 <vcsab05@gmail.com>
2015-12-11 18:47:28 +0100

    focus
    logo

---
+++ Assets/Fade.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour {
+
+	public void FadeMe () {
+		StartCoroutine (DoFade());
+	}
+
+	IEnumerator DoFade () {
+		CanvasGroup canvasGrouop = GetComponent<CanvasGroup> ();
+		while (canvasGrouop.alpha>0) {
+			canvasGrouop.alpha -= Time.deltaTime*4 ;
+			yield return null;
+			
+		}
+		canvasGrouop.interactable = false;
+		yield return null;
+	}
+}
